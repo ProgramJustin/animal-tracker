096c94c8375bb1e4daaa72fc8b0a0e2a99a8bf55
Justin Gomez <programjustin@gmail.com>
2017-07-15 11:05:00 -0700

    add array, push new animal to the array

--- app/add-animal.ts
+++ app/add-animal.ts
@@ -1,8 +1,9 @@
 class Animal {
   added: boolean = false;
 
-  
-  constructor(public species: string, public name: string, public age: number, public diet: string, public location: string, public caretakers: number, public sex: string, public likes: string, public dislikes: string) {
 
-  }
+  constructor(public species: string, public name: string, public age: number, public diet: string, public location: string, public caretakers: number, public sex: string, public likes: string, public dislikes: string) {}
+
 }
+var animals: Animal[] = [];
+animals.push(new Animal('Giraffe', 'Jeffrey', 1, 'leaves and bananas', 'Africa', 4, 'male', 'apples', 'The Rain'));
