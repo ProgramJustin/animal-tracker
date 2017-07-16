e26084d7380eba3ccb0cd5096003f8244b4f9793
Justin Gomez <programjustin@gmail.com>
2017-07-15 23:47:51 -0700

    add bootstrap class to each list based on animal's age

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   template: `
-  <h1>hi</h1>
-  <ul>
-       <li *ngFor="let currentAnimal of animals">{{currentAnimal.species}}  {{currentAnimal.name}}</li>
-       <button (click)="editAnimal()">Edit!</button>
-     </ul>
+  <div class="container">
+    <h1>hi</h1>
+    <ul>
+      <li [class]="priorityAge(currentAnimal)" *ngFor="let currentAnimal of animals">{{currentAnimal.species}}  {{currentAnimal.name}}</li>
+      <button (click)="editAnimal()">Edit!</button>
+    </ul>
+  <div>
+
+  <h3>{{selectedAnimal.likes}}</h3>
+          <p>Animal Complete? {{selectedAnimal.added}}</p>
+          <h3>Edit Animal</h3>
+          <label>Enter Animal name:</label>
+          <input [(ngModel)]="selectedAnimal.name">
+          <label>Enter Animal Priority (1-3):</label>
+          <br>
+          <input type="radio" [(ngModel)]="selectedAnimal.age" [value]="1">1 (Low Priority)<br>
+          <input type="radio" [(ngModel)]="selectedAnimal.age" [value]="2">2 (Medium Priority)<br>
+          <input type="radio" [(ngModel)]="selectedAnimal.age" [value]="3">3 (High Priority)
+       </div>
+     </div>
 
   `
 })
@@ -22,6 +38,15 @@ export class AppComponent {
 
   editAnimal() {
     alert("you");
+  }
+  priorityAge(currentAnimal){
+    if (currentAnimal.age === 3){
+      return "bg-danger";
+    } else if (currentAnimal.age === 2) {
+      return  "bg-warning";
+    } else {
+      return "bg-info";
+    }
   }
 
   isAdded(clickedAnimal: AppComponent) {
