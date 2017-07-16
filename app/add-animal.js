1260fa93c0bd18e7300d3cb8c3b1c57a75105914
Justin Gomez <programjustin@gmail.com>
2017-07-15 22:52:45 -0700

    move model to appcomponent file because files are not yet linked

--- app/add-animal.js
+++ app/add-animal.js
@@ -1,18 +1,3 @@
-var Animal = (function () {
-    function Animal(species, name, age, diet, location, caretakers, sex, likes, dislikes) {
-        this.species = species;
-        this.name = name;
-        this.age = age;
-        this.diet = diet;
-        this.location = location;
-        this.caretakers = caretakers;
-        this.sex = sex;
-        this.likes = likes;
-        this.dislikes = dislikes;
-        this.added = false;
-    }
-    return Animal;
-}());
-var animals = [];
-animals.push(new Animal('Giraffe', 'Jeffrey', 1, 'leaves and bananas', 'Africa', 4, 'male', 'apples', 'The Rain'));
-console.log(animals);
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//# sourceMappingURL=add-animal.js.map
--- app/app.component.ts
+++ app/app.component.ts
@@ -3,9 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
+  <h1>hi</h1>
+  <ul>
+       <li *ngFor="let currentAnimal of animals">{{currentAnimal.description}}</li>
+     </ul>
 
   `
 })
 export class AppComponent {
-  
+  added: boolean = false;
+  animals: Animal[] = [
+    new Animal('Giraffe', 'Jeffrey', 1, 'leaves and bananas', 'Africa', 4, 'male', 'apples', 'loud noises'),
+    new Animal('Snake', 'Kai', 1, 'Rats', 'India', 4, 'female', 'lounging', 'cold weather'),
+    new Animal('Reptile', 'Zilla', 2, 'fruits and veggies', 'Mexico', 4, 'male', 'Back Rubs', 'Iceburg Lettuce')
+  ];
+
+}
+
+
+export class Animal {
+
+  constructor(public species: string, public name: string, public age: number, public diet: string, public location: string, public caretakers: number, public sex: string, public likes: string, public dislikes: string) {}
+
+
 }
