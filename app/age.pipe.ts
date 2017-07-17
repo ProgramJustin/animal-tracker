f61019d9ea9c57b8075c057fcf2b292a63a62a9a
Justin Gomez <programjustin@gmail.com>
2017-07-16 23:11:35 -0700

    add pipe, pushes animal to array based on if its beed added or not

---
+++ app/age.pipe.ts
@@ -0,0 +1,20 @@
+import {Pipe, PipeTransform} from '@angular/core';
+import {Animal} from './animal.model';
+
+@Pipe({
+  name: "age",
+  pure: false
+})
+
+
+export class AgePipe implements PipeTransform {
+  transform(input: Animal[]){
+    var output: Animal[] = [];
+    for (var i = 0; i < input.length; i++) {
+      if (input[i].added === false) {
+        output.push(input[i]);
+      }
+    }
+    return output;
+  }
+}
--- app/app.module.ts
+++ app/app.module.ts
@@ -5,13 +5,16 @@ import { FormsModule }  from '@angular/forms';
 import { AnimalListComponent } from './animal-list.component';
 import { EditAnimalComponent } from './edit-animal.component';
 import { NewAnimalComponent } from './new-animal.component'
+import { AgePipe } from './age.pipe';
 
 @NgModule({
   imports: [ BrowserModule, FormsModule ],
   declarations: [ AppComponent,
                   AnimalListComponent,
                   EditAnimalComponent,
-                  NewAnimalComponent
+                  NewAnimalComponent,
+                  AgePipe
+
                 ],
   bootstrap:    [ AppComponent ]
 })
